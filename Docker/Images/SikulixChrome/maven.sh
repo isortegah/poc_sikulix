6fc2079b29c8de6147793bfa8f82162e6cc712c8
isortegah <isortegah@gmail.com>
2018-06-14 16:56:02 -0500

    docker file

--- Docker/Images/SikulixChrome/maven.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn verify -DENV_URL=http://google.com -Dcucumber.options="--tags @WebSikulix" -DUSER=p
---
+++ entry_point.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source /opt/bin/functions.sh
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+SERVERNUM=$(get_server_num)
+
+Xvfb $DISPLAY -screen 0 $GEOMETRY &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+for i in $(seq 1 10)
+do
+  xdpyinfo -display $DISPLAY >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo Waiting xvfb...
+  sleep 0.5
+done
+
+fluxbox -display $DISPLAY &
+
+x11vnc -forever -usepw -shared -ncache 10 -rfbport 5900 -display $DISPLAY &
+# without password VNC
+# x11vnc -forever -shared -rfbport 5900 -display $DISPLAY &
+echo "VNC in port 5900"
+
+cd /home/sikuser/poc
+mvn test
+wait $NODE_PID
